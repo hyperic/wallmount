2af0cb5970c0782607ec291fd3420222a6b62f76
jvalkeal <janne.valkealahti@gmail.com>
2011-02-13 14:08:43 +0000

    testing to explicitely define which tree nodes should dnd, for now just enable it to metric nodes.

--- dojo-release-1.5.0-src/hyperic/data/TreeDndSource.js
+++ dojo-release-1.5.0-src/hyperic/data/TreeDndSource.js
@@ -4,6 +4,24 @@ dojo.require("dijit.tree.dndSource");
 
 dojo.declare("hyperic.data.TreeDndSource", dijit.tree.dndSource, {
 	
-	copyOnly:true
+	copyOnly:true,
+	
+    onMouseMove: function(e){
+    	// overwrite parents onMouseMove to explicitely check
+    	// whether node should be dragged.
+    	// For now, just enable dnd if node is a metric.
+        if(this.isDragging && this.targetState == "Disabled"){ return; }
+       	if(this.mouseDown) {
+            var _nodes = this.anchor;               //get the dragged tree row's div
+            var _id = _nodes.id;                    //get the id of the dragged div
+            var dragDndItem = this.getItem(_id);    //get the dnd item for the dragged div
+            var dragTreeNode = dragDndItem.data;    //get the treenode of the dragged div
+            var dragItem = dragTreeNode.item;       //get the store item bound to the dragged treenode
+            if(dragItem.mid){
+                this.inherited(arguments);
+            }
+        }        	
+    }
+	
 	
 });
