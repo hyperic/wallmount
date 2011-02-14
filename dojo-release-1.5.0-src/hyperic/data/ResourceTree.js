4fe6289e76c1435eecb7068abcfa687baf1101b9
jvalkeal <janne.valkealahti@gmail.com>
2011-02-14 10:30:19 +0000

    updates to tree icon handling. also updates to related test files.

--- dojo-release-1.5.0-src/hyperic/data/ResourceTree.js
+++ dojo-release-1.5.0-src/hyperic/data/ResourceTree.js
@@ -5,42 +5,26 @@ dojo.require("dijit.tree.dndSource");
 
 dojo.declare("hyperic.data.ResourceTree", dijit.Tree, {
 
-//    dndParams: ["onDndDrop","itemCreator","onDndCancel","checkAcceptance", "checkItemAcceptance", "dragThreshold", "betweenThreshold", "copyOnly"],
-
-//    constructor: function(){
-//    	this.dndController = new dijit.tree.dndSource(this,{copyOnly:true});
-//    },
-    
-    /*=====
-    We don't want to dnd inside the tree. 
-    =====*/
     checkAcceptance: function(source, nodes){
+    	// We don't want to dnd inside the tree.
     	return false;
     },
 
     getIconClass: function(/*dojo.data.Item*/ item, /*Boolean*/ opened){
-    	// XXX: redo this
-    	if(item.id){
-    	   if(item.id.indexOf("r-1") === 0)
-               return "hypericIconPlatform";
-           else if(item.id.indexOf("m") === 0)
-               return "hypericIconMetric";
-           else if(item.id.indexOf("3") === 0)
-               return "hypericIconService";
-           else if(item.id.indexOf("r-3") === 0)
-               return "hypericIconService";
-    	}
-
-        if(item.$ref){
-          if(item.$ref.indexOf("r-1") === 0)
-               return "hypericIconPlatform";
-           else if(item.$ref.indexOf("m") === 0)
-               return "hypericIconMetric";
-           else if(item.$ref.indexOf("3") === 0)
-               return "hypericIconService";
-           else if(item.$ref.indexOf("r-3") === 0)
-               return "hypericIconService";
+    	// define tree icons
+    	
+        if(item.eid){
+           if(item.eid.indexOf("1:") === 0)
+               return "hypericIconPlatform";        	
+           if(item.eid.indexOf("2:") === 0)
+               return "hypericIconServer";            
+           if(item.eid.indexOf("3:") === 0)
+               return "hypericIconService";            
         }
+    	
+        if(item.mid){
+        	return "hypericIconMetric";
+        }    	
     	           
         return (!item || this.model.mayHaveChildren(item)) ? (opened ? "dijitFolderOpened" : "dijitFolderClosed") : "dijitLeaf"
     }
