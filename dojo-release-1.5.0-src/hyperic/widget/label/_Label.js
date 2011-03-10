6d5711eb2f15c34b3bd94acf3977d510f0d9bdd3
jvalkeal <janne.valkealahti@gmail.com>
2011-03-10 20:28:27 +0000

    adding ranges to label components.

--- dojo-release-1.5.0-src/hyperic/widget/label/_Label.js
+++ dojo-release-1.5.0-src/hyperic/widget/label/_Label.js
@@ -4,14 +4,21 @@ dojo.require("hyperic.widget.base._WallMountItem");
 dojo.require("hyperic.util.FontUtil");
 dojo.require("hyperic.unit.UnitsConvert");
 dojo.require("hyperic.data.LabelProperty");
+dojo.require("hyperic.data.RangesProperty");
 
-dojo.declare("hyperic.widget.label._Label",[
-    hyperic.widget.base._WallMountItem,
-    hyperic.data.LabelProperty],{
-    
+dojo.declare("hyperic.widget.label._Label",
+    [ hyperic.widget.base._WallMountItem,
+     hyperic.data.LabelProperty,
+     hyperic.data.RangesProperty ],{
+     	
+    // internal data
+    _rcolor:null, // caching color, either from main or from ranges
+     	
     drawMetric: function(){
         // summary:
         //     draws formatted metric value between spinning arrows
+        
+        this._cacheColor();
         
         var fV = hyperic.unit.UnitsConvert.convert(this.value, this.format);        
         
@@ -19,7 +26,18 @@ dojo.declare("hyperic.widget.label._Label",[
         // seem to get pretty good result
         var fS = hyperic.util.FontUtil.findGoodSizeFontByRect(fV, this.width-(this.width/10), this.height);
         var shift = Math.round(fS * 0.34);
-        this.drawText(fV, this.width/2, this.height/2 + shift , "middle", this.getLabelColor(), {family:"Helvetica",weight:"bold",size:fS+'px'});
+        this.drawText(fV, this.width/2, this.height/2 + shift , "middle", this._rcolor, {family:"Helvetica",weight:"bold",size:fS+'px'});
+    },
+    
+    resetValue: function(){
+    	this._cacheColor();
+    	this.inherited(arguments);
+    },
+
+    _cacheColor: function(){
+        var range = this.getInRange(this.value);
+        this._rcolor = this.getLabelColor()
+        if(range != null) this._rcolor = range.color;       
     },
 
     asParams: function(){
@@ -28,6 +46,7 @@ dojo.declare("hyperic.widget.label._Label",[
         
         var paramObj = this.inherited(arguments);
         paramObj['labelColor'] = this.getLabelColor();
+        paramObj['ranges'] = this.asRangesParams();
         return paramObj;
     }
         
